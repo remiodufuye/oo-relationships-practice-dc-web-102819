df1c24c763a0800477e9a2b70a9d4e6937fbd4b4
remiodufuye <remi.odufuye@gmail.com>
2019-11-09 16:31:30 -0500

    chanes made

--- tools/console.rb
+++ tools/console.rb
@@ -4,31 +4,10 @@ def reload
   load 'config/environment.rb'
 end
 
-##### LISTING ############
-
-bigbro = Listing.new("Big Brother House" , "Los Angeles") 
-safehouse  = Listing.new("Safe Haven" , "Los Angeles")
-funfactory = Listing.new("Fun Factory" , "New Mexico") 
-
-##### GUEST ######
-
-yinca = Guest.new("Yinca")
-remi = Guest.new("Remi")
-bose = Guest.new("Bose") 
 
 
-####### TRIPS ##########
-
-trip1 = Trip.new(yinca , bigbro)
-trip2 = Trip.new(remi , bigbro)
-trip3 = Trip.new(bose, bigbro)
-trip4 = Trip.new(yinca, safehouse)
-trip4 = Trip.new(remi , funfactory) 
-trip5 = Trip.new(bose , funfactory) 
 
 
-puts yinca.trips 
-puts bigbro.trip_count 
 
 binding.pry
 0  
